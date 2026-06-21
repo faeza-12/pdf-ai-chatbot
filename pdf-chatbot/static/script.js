29d8f314faed92542b87d30885b818da64e744ba
Faeza <faezafarha07@gmail.com>
2026-06-21 21:24:36 +0530

    fix upload url

--- pdf-chatbot/static/script.js
+++ pdf-chatbot/static/script.js
@@ -64,7 +64,7 @@ async function askQuestion() {
     try {
 
         const response = await fetch(
-            "http://127.0.0.1:8000/ask",
+            "/ask",
             {
                 method: "POST",
                 headers: {
